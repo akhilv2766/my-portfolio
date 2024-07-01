8042584f2df96c6df687be8d4b52387d16b343ec
akhilv2766 <akhilv6080@gmail.com>
2024-07-01 11:42:15 +0530

    FINAL

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 AOS.init();
+
+$(document).ready(function(){
+
+    $(".logo").hover(function(){
+        $(this).css({
+            'transition': 'transform 1000ms',
+            'transform': 'scale(1.5)'
+        });
+    },function(){
+        $(this).css({
+            'transition': 'transform 500ms',
+            'transform': 'scale(1)'
+        });
+    });
+
+
+
+});
